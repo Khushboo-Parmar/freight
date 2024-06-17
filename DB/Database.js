f183ec6e8220cfdffafff0a219ed4c9d7af74375
Khushboo-Parmar <164618523+Khushboo-Parmar@users.noreply.github.com>
2024-06-17 19:45:06 +0530

    Update Database.js

--- DB/Database.js
+++ DB/Database.js
@@ -1,52 +1,13 @@
-// const mysql = require('mysql');
-// const con = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "freight"
-//   });
-
-//   con.connect(function (err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//   });
-//   module.exports = con;
-
-// const mysql = require('mysql');
-
-// const con = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "freight"
-// });
-
-// con.connect(function (err) {
-//     if (err) {
-//         console.error('Error connecting to database:');
-//         console.error(err.stack);
-//         return;
-//     }
-//     console.log('Connected to MySQL server as id ' + con.threadId);
-// });
-
-// module.exports = con;
-
 const mysql = require('mysql');
 
-<<<<<<< HEAD
 const pool = mysql.createPool({
     connectionLimit: 10, // adjust as per your application's needs
-=======
-const con = mysql.createConnection({
->>>>>>> 9b7941321a2fdc385f1524d75e8b26bff3706d56
     host: "localhost",
     user: "root",
     password: "",
     database: "freight"
 });
 
-<<<<<<< HEAD
 pool.getConnection((err, connection) => {
     if (err) {
         console.error('Error connecting to database:', err);
@@ -67,15 +28,3 @@ pool.getConnection((err, connection) => {
 });
 
 module.exports = pool;
-=======
-con.connect(function (err) {
-    if (err) {
-        console.error('Error connecting to database:');
-        console.error(err.stack);
-        return;
-    }
-    console.log('Connected to MySQL server as id ' + con.threadId);
-});
-
-module.exports = con;
->>>>>>> 9b7941321a2fdc385f1524d75e8b26bff3706d56
