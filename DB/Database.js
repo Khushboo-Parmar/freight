f192938a9f216fbc6bf8f6457a59a3b90e8e35de
Khushboo-Parmar <164618523+Khushboo-Parmar@users.noreply.github.com>
2024-06-17 15:46:48 +0530

    Update Database.js

--- DB/Database.js
+++ DB/Database.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql');
+
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "freight"
-  });
+});
 
-  con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-  module.exports = con;
+con.connect(function (err) {
+    if (err) {
+        console.error('Error connecting to database:');
+        console.error(err.stack);
+        return;
+    }
+    console.log('Connected to MySQL server as id ' + con.threadId);
+});
+
+module.exports = con;
