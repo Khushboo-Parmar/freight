e2d7c3614cadbe4cf31cacbd75b7b6244120d7cc
Khushboo-Parmar <164618523+Khushboo-Parmar@users.noreply.github.com>
2024-06-27 18:58:29 +0530

    Update Database.js

--- DB/Database.js
+++ DB/Database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
     connectionLimit: 10, // adjust as per your application's needs
     host: "138.197.7.241",
-    user: "root",
-    password: "sws@2026",
+    user: "swsteam",
+    password: "Sws@root@2024",
     database: "freight"
 });
 
